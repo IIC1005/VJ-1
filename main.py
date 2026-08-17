19f5694e3fd1edf2d912a19fe3aa89daf59bdba1
damiangubbins <damiangubbins@gmail.com>
2026-08-17 19:49:19 -0400

    blu

--- main.py
+++ main.py
@@ -2,17 +2,17 @@ import pygame
 
 from scenes import basic_scene, game_scene
 
-# Inicializamos pygame
+# ? Inicializamos pygame
 pygame.init()
 
-# Definimos las medidas de nuestra pantalla
+# ? Definimos las medidas de nuestra pantalla
 SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 700
 
-# Creamos nuestro objeto pantalla
+# ? Creamos nuestro objeto pantalla
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-# Aqui se ejecutaran las escenas del juego en orden
+# ? Aqui se ejecutaran las escenas del juego en orden
 basic_scene.gameloop(screen)
 
 # TODO (2.2): Añadir nueva escena de juego
